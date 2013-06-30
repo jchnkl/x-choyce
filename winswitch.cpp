4dbee15776c19c49463da92ff90b60d7330baac4
Jochen Keil <jochen.keil@gmail.com>
2013-06-30 22:27:52 +0200

    Fix mistake from rebasing/commit writing: code duplication

--- winswitch.cpp
+++ winswitch.cpp
@@ -746,48 +746,18 @@ class grid_t : public layout_t {
 
       int i = 0;
       for (auto & client : clients) {
-        double scale_x = (double)rects[i].width() / (double)client.rectangle().width();
-        double scale_y = (double)rects[i].height() / (double)client.rectangle().height();
+        double scale_x = (double)rects[i].width()
+                       / (double)client.rectangle().width();
+        double scale_y = (double)rects[i].height()
+                       / (double)client.rectangle().height();
         client.preview_scale() = std::min(scale_x, scale_y);
         client.preview_position().x = rects[i].x();
         client.preview_position().y = rects[i].y();
 
-        unsigned int realwidth  = client.rectangle().width() * client.preview_scale();
-        unsigned int realheight = client.rectangle().height() * client.preview_scale();
-
-        if (realwidth < rects[i].width()) {
-          client.preview_position().x += (rects[i].width() - realwidth) / 2;
-        }
-        if (realheight < rects[i].height()) {
-          client.preview_position().y += (rects[i].height() - realheight) / 2;
-        }
-        i++;
-      }
-
-      int ncol = cells.size();
-      int colw = screen.width() / ncol;
-
-      std::vector<rectangle_t> rects;
-      for (int c = 0; c < ncol; ++c) {
-        int nrow = cells[c];
-        int rowh = screen.height() / nrow;
-        for (int r = 0; r < nrow; ++r) {
-          rects.push_back(rectangle_t(c * colw + screen.x() + gap,
-                                      r * rowh + screen.y() + gap,
-                                      colw - 2 * gap, rowh - 2 * gap));
-        }
-      }
-
-      int i = 0;
-      for (auto & client : clients) {
-        double scale_x = (double)rects[i].width() / (double)client.rectangle().width();
-        double scale_y = (double)rects[i].height() / (double)client.rectangle().height();
-        client.preview_scale() = std::min(scale_x, scale_y);
-        client.preview_position().x = rects[i].x();
-        client.preview_position().y = rects[i].y();
-
-        unsigned int realwidth  = client.rectangle().width() * client.preview_scale();
-        unsigned int realheight = client.rectangle().height() * client.preview_scale();
+        unsigned int realwidth  = client.rectangle().width()
+                                * client.preview_scale();
+        unsigned int realheight = client.rectangle().height()
+                                * client.preview_scale();
 
         if (realwidth < rects[i].width()) {
           client.preview_position().x += (rects[i].width() - realwidth) / 2;
