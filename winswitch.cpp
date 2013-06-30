f9e864c8a0081ae1c5b86a8ae29d251a6e79ed61
Jochen Keil <jochen.keil@gmail.com>
2013-06-30 22:10:46 +0200

    Class which actually implements the presentation of client previews
    Uses layout_t interface for arranging clients
    Keeps iterator for x_client_container class
    Handles keyboard {un,}grabbing

--- winswitch.cpp
+++ winswitch.cpp
@@ -813,6 +813,12 @@ class grid_t : public layout_t {
     }
 };
 
+class x_clients_preview : public x_event_handler {
+  public:
+    x_clients_preview(const x_connection & c,
+                      const layout_t * layout,
+                      x_client_container & x_clients)
+      : _c(c), _layout(layout), _x_clients(x_clients) {}
 
     void handle(xcb_generic_event_t * ge)
     {
@@ -820,31 +826,69 @@ class grid_t : public layout_t {
         xcb_key_press_event_t * e = (xcb_key_press_event_t *)ge;
         xcb_keysym_t keysym = _c.keycode_to_keysym(e->detail);
         if (keysym == XK_Escape && e->state == 0) {
-          std::exit(EXIT_SUCCESS);
-        } else if (keysym == XK_Tab && e->state == XCB_MOD_MASK_4) {
-          _c.grab_keyboard();
-          std::cerr << "XK_Tab + XCB_MOD_MASK_4" << std::endl;
+          _active = false;
+
+        } else if (keysym == XK_Tab
+            && (e->state == XCB_MOD_MASK_4
+              || e->state == (XCB_MOD_MASK_4 | XCB_MOD_MASK_SHIFT))) {
+          if (_active) {
+            _current_client->update_preview(false);
+            move_client(e->state == XCB_MOD_MASK_4);
+            _current_client->update_preview(true);
+          } else {
+            _active = true;
+            _c.grab_keyboard();
+            _active_window = _c.net_active_window();
+            _x_clients.update();
+
+            _layout->arrange(_c.current_screen(), _x_clients);
+
+            _current_client =
+              std::find(_x_clients.begin(), _x_clients.end(), _active_window);
+            move_client(e->state == XCB_MOD_MASK_4);
+
+            for (auto & client : _x_clients) {
+              client.show_preview(client == _current_client->window());
+            }
+          }
         }
 
       } else if (XCB_KEY_RELEASE == (ge->response_type & ~0x80)) {
         xcb_key_release_event_t * e = (xcb_key_release_event_t *)ge;
         xcb_keysym_t keysym = _c.keycode_to_keysym(e->detail);
         if (keysym == XK_Super_L) {
+          for (auto & client : _x_clients) {
+            client.hide_preview();
+          }
+          _c.request_change_active_window(_current_client->window());
+          _active = false;
           _c.ungrab_keyboard();
-          std::cerr << "release" << std::endl;
         }
       }
     }
 
   private:
-    container_t _x_clients;
-    std::vector<xcb_window_t> _windows;
-    const x_connection & _c;
-    x_event_source & _x_event_source;
-};
+    bool _active = false;
+    xcb_window_t _active_window;
 
-  private:
     const x_connection & _c;
+    const layout_t * _layout;
+    x_client_container & _x_clients;
+    x_client_container::iterator _current_client;
+
+    void move_client(bool next)
+    {
+      if (next) {
+        if (++_current_client == _x_clients.end()) {
+          _current_client = _x_clients.begin();
+        }
+      } else {
+        if (_current_client == _x_clients.begin()) {
+          _current_client = _x_clients.end();
+        }
+        --_current_client;
+      }
+    }
 };
 
 xcb_render_pictformat_t
