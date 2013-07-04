8a7e96b2dc4aff7c19e470871f5b35ad61320a78
Jochen Keil <jochen.keil@gmail.com>
2013-07-04 10:32:34 +0200

    Rewrite main to reflect latest changes

--- main.cpp
+++ main.cpp
@@ -1,34 +1,23 @@
-#include <xcb/xcb.h>
 #include <X11/keysym.h>
 
-#include "data_types.hpp"
-#include "x_event_handler_t.hpp"
-#include "x_connection.hpp"
-#include "x_client.hpp"
-#include "x_event_source.hpp"
-#include "x_client_container.hpp"
-#include "x_client_chooser.hpp"
-#include "layout_t.hpp"
 #include "grid.hpp"
+#include "x_connection.hpp"
+#include "x_client_chooser.hpp"
+#include "x_client_thumbnail_manager.hpp"
 
 int main(int argc, char ** argv)
 {
   xcb_keysym_t key = XK_Tab;
-  xcb_mod_mask_t mod = XCB_MOD_MASK_1;
+  xcb_mod_mask_t mod = XCB_MOD_MASK_4;
 
   x_connection c;
 
-  x_event_source es(c);
-  x_client_container cc(c, es);
-  es.register_handler(&cc);
-
   grid_t grid;
-  x_client_chooser cp(c, &grid, cc, key, mod);
+  x_client_thumbnail_manager tm(c, &grid);
 
-  es.register_handler(&c);
-  es.register_handler(&cp);
+  x_client_chooser cp(c, &tm, key, mod);
 
-  es.run_event_loop();
+  c.run_event_loop();
 
   return 0;
 }
