04c5956e00877de3ba1754364355021db6ca4814
Jochen Keil <jochen.keil@gmail.com>
2013-11-02 12:58:10 +0100

    Make thumbnail_container_t a real type instead of a typedef

--- thumbnail_t.hpp
+++ thumbnail_t.hpp
@@ -11,7 +11,18 @@ class thumbnail_t {
     virtual void hide(void) = 0;
     virtual void update(void) = 0;
     virtual void select(void) = 0;
+    virtual const unsigned int & window(void) = 0;
     virtual void highlight(bool want_highlight) = 0;
+};
+
+template<template<class t = thumbnail_t::thumbnail_ptr,
+                  class = std::allocator<t>>
+         class container_t>
+class thumbnail_container_t {
+  public:
+    virtual void notify(void) = 0;
+    virtual container_t<thumbnail_t::thumbnail_ptr> & operator*(void) = 0;
+    virtual container_t<thumbnail_t::thumbnail_ptr> * operator->(void) = 0;
 };
 
 template<template<class t = thumbnail_t::thumbnail_ptr,
@@ -19,12 +30,11 @@ template<template<class t = thumbnail_t::thumbnail_ptr,
          class container_t>
 class thumbnail_factory_t {
   public:
-    typedef container_t<thumbnail_t::thumbnail_ptr> thumbnail_container_t;
     typedef std::back_insert_iterator<container_t<thumbnail_t::thumbnail_ptr>>
       back_insert_iterator;
 
     virtual void make(back_insert_iterator insert) = 0;
-    virtual void manage(unsigned int id, thumbnail_container_t & container) = 0;
+    virtual void manage(unsigned int id, thumbnail_container_t<container_t> * container) = 0;
     virtual void giveup(unsigned int id) = 0;
     virtual void update(unsigned int id) = 0;
 };
