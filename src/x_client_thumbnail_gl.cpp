a9a0fbd9c1c8991694eea4ca473c62254c0fb7d8
Jochen Keil <jochen.keil@gmail.com>
2013-12-07 23:35:06 +0100

    gl::context::texture disables the texture unit afterwards, so use active_texture

--- src/x_client_thumbnail_gl.cpp
+++ src/x_client_thumbnail_gl.cpp
@@ -317,7 +317,6 @@ x_client_thumbnail::handle(xcb_generic_event_t * ge)
             m_gl_api.glXReleaseTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT);
             m_gl_api.glXBindTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT, NULL);
           });
-
           m_gl_api.glGenerateMipmap(GL_TEXTURE_2D);
         }
 
@@ -378,27 +377,22 @@ x_client_thumbnail::configure_highlight(bool now)
 
   if (m_highlight) {
     use_program("normal_shader");
-    m_gl_ctx.texture(0, [this](const GLuint &)
-    {
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1);
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-      m_gl_api.glGenerateMipmap(GL_TEXTURE_2D);
-    });
+    m_gl_ctx.active_texture(0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    m_gl_api.glGenerateMipmap(GL_TEXTURE_2D);
 
   } else {
     use_program("grayscale_shader");
-    m_gl_ctx.texture(0, [this](const GLuint &)
+    m_gl_ctx.active_texture(0);
+    m_gl_ctx.pixmap(0, [this](const GLXPixmap & p)
     {
-      m_gl_ctx.pixmap(0, [this](const GLXPixmap & p)
-      {
-        m_gl_api.glXReleaseTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT);
-        m_gl_api.glXBindTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT, NULL);
-      });
-
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-      glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      m_gl_api.glXReleaseTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT);
+      m_gl_api.glXBindTexImageEXT(m_c.dpy(), p, GLX_FRONT_EXT, NULL);
     });
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   }
 }
 
