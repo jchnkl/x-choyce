cd23c420e129b645111274dd69c199b83eedff1a
Jochen Keil <jochen.keil@gmail.com>
2013-12-07 20:47:05 +0100

    Add methods for setting fg/bg and font

--- src/x_xft.hpp
+++ src/x_xft.hpp
@@ -34,15 +34,63 @@ class xft {
 
     XftFont * const operator[](const x::type::fontname & fontname);
 
-  private:
-    xft(const xft & other);
-    xft & operator=(const xft & other);
+    const Drawable & drawable(void)
+    {
+      return m_drawable;
+    }
 
+    xft & fontname(const x::type::fontname & font)
+    {
+      m_font = *(*this)[font];
+      return *this;
+    }
+
+    xft & foreground(const x::type::colorname & foreground)
+    {
+      m_fg_color = *(*this)[foreground];
+      return *this;
+    }
+
+    xft & background(const x::type::colorname & background)
+    {
+      m_fg_color = *(*this)[background];
+      return *this;
+    }
+
+    xft & fg_alpha(const double & a)
+    {
+      m_fg_color.color.alpha *= a;
+      return *this;
+    }
+
+    xft & bg_alpha(const double & a)
+    {
+      m_bg_color.color.alpha *= a;
+      return *this;
+    }
+
+    xft & fill(unsigned int x = 0, unsigned int y = 0,
+               unsigned int width = 0, unsigned int height = 0)
+    {
+      width = width == 0 ? m_width : width;
+      height = height == 0 ? m_height : height;
+      XftDrawRect(m_xftdraw, &m_bg_color, x, y, m_width, m_height);
+      return *this;
+    }
+
+  private:
     XftDraw * m_xftdraw;
     Display * m_dpy;
     Drawable m_drawable;
     Colormap m_colormap;
     XVisualInfo * m_visual_info;
+
+    unsigned int m_width = 0;
+    unsigned int m_height = 0;
+
+    XftFont m_font;
+    XftColor m_fg_color;
+    XftColor m_bg_color;
 
     std::unordered_map<std::string, XftColor> m_colors;
     std::unordered_map<std::string, XftFont *> m_fonts;
