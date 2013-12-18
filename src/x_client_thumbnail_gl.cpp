631eeca85af156f1d9c0bd390872fcdf51e4eaef
Jochen Keil <jochen.keil@gmail.com>
2013-12-18 21:13:16 +0100

    Remove superfluous function call

--- src/x_client_thumbnail_gl.cpp
+++ src/x_client_thumbnail_gl.cpp
@@ -214,8 +214,6 @@ x_client_thumbnail::update(int x, int y, unsigned int width, unsigned int height
 
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-  glLoadIdentity();
-
   glPushMatrix();
   glBegin(GL_QUADS);
   glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0, 0.0);
